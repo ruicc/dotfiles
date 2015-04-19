7b85b0a938bb7cfbf49862d63c6eea4808c580d8
Kousuke Ruichi <ruicc.rail@gmail.com>
2015-04-19 13:29:43 +0900

    Add settings to karabiner.

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -12,9 +12,11 @@ $cli set repeat.initial_wait 300
 /bin/echo -n .
 $cli set option.emacsmode_controlLeftbracket 1
 /bin/echo -n .
+$cli set option.emacsmode_ex_controlU_delete 1
+/bin/echo -n .
 $cli set remap.space2shiftL_space_keyrepeat 1
 /bin/echo -n .
-$cli set option.emacsmode_ex_controlU_delete 1
+$cli set option.emacsmode_controlPNBF_nomodifiers 1
 /bin/echo -n .
 $cli set remap.swap_number_and_symbol 1
 /bin/echo -n .
